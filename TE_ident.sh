d2ac35ce018072a49480343d81e2b66223fe2f4d
Mark Whitehead <hlmwhite@liv.ac.uk>
2026-08-21 10:49:41 +0100

    Delete TE_ident.sh

--- TE_ident.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-T_PSI_REF_PATH=$(readlink -f $1)
-
-diamond blastp --sensitive --query ../evm/EVM_out.EVM.pep --out repeats.out -d $T_PSI_REF_PATH --evalue 1e-10 --max-target-seqs 1 --outfmt 6 --header
-
-grep -v -f <(cat repeats.out | grep -v '#'  | cut -f 1 | sed -e 's/evm.model.//' -e 's/$/;/') ../evm/EVM_out.EVM.gff3 | grep -v -f <(cat repeats.out | grep -v '#'  | cut -f 1 | sed -e 's/evm.model.//' -e 's/$/\.exon/') - > noTE.EVM.gff3
-
-
